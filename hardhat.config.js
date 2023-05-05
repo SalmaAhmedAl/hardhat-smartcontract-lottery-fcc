161a6c34debff471437b880f18ad8a1567dc6004
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-05-05 16:14:05 +0300

    [issue solved] config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,44 +7,57 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 const SEPOLIA_RPC_URL =
-    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/3cQUJzAdZHrMRoKunp7noKgiNN8o3b5L"
+    process.env.SEPOLIA_RPC_URL ||
+    "https://eth-sepolia.g.alchemy.com/v2/3cQUJzAdZHrMRoKunp7noKgiNN8o3b5L"
 const PRIVATE_KEY = "fcfebbb7539d8e6c69f622e5a3bd5286bcab34b3fd9be64936289f6e602d9184"
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 module.exports = {
-    defaultNetwork:"hardhat",
-    networks:{
+    defaultNetwork: "hardhat",
+    defaultNetwork: "hardhat",
+    networks: {
         hardhat: {
             // // If you want to do some forking, uncomment this
             // forking: {
             //   url: MAINNET_RPC_URL
             // }
             chainId: 31337,
-            blockConfirmations:1,
+        },
+        localhost: {
+            chainId: 31337,
         },
         sepolia: {
             url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
             chainId: 11155111,
-            blockConfirmatiions: 6,
-            gas: 2100000,
-            gasPrice: 8000000000,
         },
-    etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
     },
 
-    gasReporter: {
-        enabled: true,
-        noColors: true,
-        currency: "USD",
-        outputFile: "gas_report.txt",
-        //coinmarketcap:COINMARKETCAP_API_KEY,
-        token: "ETH",
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
     },
-    solidity: "0.8.7",
+    gasReporter: {
+        outputFile: "gas_report.txt",
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+       // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+       token: "ETH",
+
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Raffle"],
+    },
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
@@ -54,10 +67,18 @@ module.exports = {
             default: 1,
         },
     },
-    
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+
     mocha: {
         timeout: 500000, // 500 seconds max for running tests
     },
-   
-    }
 }
